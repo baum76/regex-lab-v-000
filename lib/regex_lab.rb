bbba7a7b00fa85576707c1a08e3878d405af35b6
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-28 19:29:00 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,8 @@
 def starts_with_a_vowel?(word)
   if word.match(/\A[aeiou]/i)
-    return false
+    return true
   else
-    return true  
+    return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
